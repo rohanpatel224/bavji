6b30890967c3e62a1acf668b1ecb496aeee6276f
Rp-NB <patel.rohan@nobroker.in>
2022-11-19 21:21:12 +0530

    updated routes

--- src/App.js
+++ src/App.js
@@ -5,16 +5,42 @@ import img4 from "./Images/invitation_page-0004.jpg";
 import img3 from "./Images/invitation_page-0003.jpg";
 import img2 from "./Images/invitation_page-0002.jpg";
 import img1 from "./Images/invitation_page-0001.jpg";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App" style={{ backgroundColor: "white" }}>
-      <img src={img1} alt="p1" width={"100%"} />
-      <img src={img2} alt="p2" width={"100%"} />
-      <img src={img3} alt="p3" width={"100%"} />
-      <img src={img4} alt="p4" width={"100%"} />
-      <img src={img5} alt="p5" width={"100%"} />
-      <img src={img6} alt="p6" width={"100%"} />
-    </div>
+    <>
+      <Routes>
+        <Route
+          path="/"
+          exact
+          element={
+            <div className="App" style={{ backgroundColor: "white" }}>
+              <img src={img1} alt="p1" width={"100%"} />
+              <img src={img2} alt="p2" width={"100%"} />
+              <img src={img3} alt="p3" width={"100%"} />
+              <img src={img4} alt="p4" width={"100%"} />
+              <img src={img5} alt="p5" width={"100%"} />
+              <img src={img6} alt="p6" width={"100%"} />
+            </div>
+          }
+        />
+        <Route
+          path="/hospital"
+          exact
+          element={
+            <div className="App" style={{ backgroundColor: "white" }}>
+              <img src={img1} alt="p1" width={"100%"} />
+              <img src={img2} alt="p2" width={"100%"} />
+              <img src={img3} alt="p3" width={"100%"} />
+              <img src={img4} alt="p4" width={"100%"} />
+              <img src={img5} alt="p5" width={"100%"} />
+              <img src={img6} alt="p6" width={"100%"} />
+            </div>
+          }
+        />
+      </Routes>
+    </>
   );
 }
 
