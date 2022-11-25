d8f693292a64e56ff088fb6bf31e56fccc75fdae
Rp-NB <patel.rohan@nobroker.in>
2022-11-25 23:08:07 +0530

    added patrika

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import img4 from "./Images/invitation_page-0004.jpg";
 import img3 from "./Images/invitation_page-0003.jpg";
 import img2 from "./Images/invitation_page-0002.jpg";
 import img1 from "./Images/invitation_page-0001.jpg";
+
+import img21 from "./Images/patrika/Jesingbapa Patrika_pages-to-jpg-0001.jpg";
+import img22 from "./Images/patrika/Jesingbapa Patrika_pages-to-jpg-0002.jpg";
+import img23 from "./Images/patrika/Jesingbapa Patrika_pages-to-jpg-0003.jpg";
+import img24 from "./Images/patrika/Jesingbapa Patrika_pages-to-jpg-0004.jpg";
+import img25 from "./Images/patrika/Jesingbapa Patrika_pages-to-jpg-0005.jpg";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -16,12 +22,31 @@ function App() {
           exact
           element={
             <div className="App" style={{ backgroundColor: "white" }}>
-              <img src={img1} alt="p1" width={"100%"} />
-              <img src={img2} alt="p2" width={"100%"} />
-              <img src={img3} alt="p3" width={"100%"} />
-              <img src={img4} alt="p4" width={"100%"} />
-              <img src={img5} alt="p5" width={"100%"} />
-              <img src={img6} alt="p6" width={"100%"} />
+              <h1 style={{ color: "#ff2059", textDecoration: "underline" }}>
+                શ્રીમદ્ જેશીંગબાપા શતાબ્દી મહોત્સવ
+              </h1>
+              <div>
+                <a
+                  style={{
+                    textDecoration: "none",
+                    fontSize: "25px",
+                    marginBottom: "10px",
+                    textdecoration: "none",
+                    display: "flex",
+                    justifyContent: "center",
+                  }}
+                  href="/patrika"
+                >
+                  ~ આમંત્રણ પત્રિકા
+                </a>
+                <a
+                  style={{ textDecoration: "none", fontSize: "25px" }}
+                  href="/hospital"
+                >
+                  ~ હોસ્પિટલ ઉપલબ્ધ સેવાઓ, કાર્ડ
+                </a>
+              </div>
+              <p style={{ colour: "Red" }}>open the link above</p>
             </div>
           }
         />
@@ -36,6 +61,19 @@ function App() {
               <img src={img4} alt="p4" width={"100%"} />
               <img src={img5} alt="p5" width={"100%"} />
               <img src={img6} alt="p6" width={"100%"} />
+            </div>
+          }
+        />
+        <Route
+          path="/patrika"
+          exact
+          element={
+            <div className="App" style={{ backgroundColor: "white" }}>
+              <img src={img21} alt="p1" width={"100%"} />
+              <img src={img22} alt="p2" width={"100%"} />
+              <img src={img23} alt="p3" width={"100%"} />
+              <img src={img24} alt="p4" width={"100%"} />
+              <img src={img25} alt="p5" width={"100%"} />
             </div>
           }
         />
